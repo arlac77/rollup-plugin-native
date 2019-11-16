e41e57d0d48a8e8b8632d68b35a02cde92924c6a
Markus Felten <Markus.Felten@gmx.de>
2019-11-16 02:23:54 +0100

    feat: load native lib to see actual exports

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,8 @@
 import { arch, platform } from "os";
+import Module from "module";
+import os from "os";
 
-const keys = ["LISTEN_FDS_START", "notify", "journal_print_object"];
+let keys = ["LISTEN_FDS_START", "notify", "journal_print_object"];
 
 //const archs={'x64':'x86_64','arm':'armv7l'};
 
@@ -10,7 +12,14 @@ export default function native(options = {}) {
 
     load(id) {
       if (id.endsWith(".node.resolved")) {
-        console.log("LOAD", id);
+        const filename = id.replace(".node.resolved", ".node");
+        console.log("LOAD", id, filename);
+        const m = new Module(filename);
+        m.filename = filename;
+        process.dlopen(m, m.filename, os.constants.dlopen.RTLD_NOW);
+        console.log("EXPORTS", m.exports);
+        keys = Object.keys(m.exports);
+
         return {
           code: `export { ${keys} };`,
           map: null
@@ -35,16 +44,23 @@ export default function native(options = {}) {
         let platformId;
 
         const r = id.match(/(.+)(-(\w+)-(\w+)).node.resolved$/);
-        if(r) {
-          platformId = id.replace(".node.resolved",".node");
-        }
-        else {
+        if (r) {
+          platformId = id.replace(".node.resolved", ".node");
+        } else {
           const a = arch();
-          const p = platform();  
-          platformId = id.replace(".node.resolved",`-${p}-${a}.node`);
+          const p = platform();
+          platformId = id.replace(".node.resolved", `-${p}-${a}.node`);
         }
 
         return {
+          /*code: `
+          const Module = require('module');
+          const os = require('os');
+          const m = new Module(filename);
+          m.filename = filename;
+          process.dlopen(m, m.filename,os.constants.dlopen.RTLD_NOW);
+          `,
+          */
           code: `
 const { createRequire } = require("module");
 const { ${keys} } = createRequire(import.meta.url)("${platformId}");
